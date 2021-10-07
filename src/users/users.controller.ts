3059b372da3f922da507ccb0c35c23f23318e2aa
rodrigorafaeldamaceno <rodrigorafaeldamaceno@gmail.com>
2021-10-07 18:45:27 -0300

    added docker and created admin register

---
+++ src/users/users.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Get, Post, ValidationPipe } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { ReturnUserDto } from './dtos/return-user.dtop';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post()
+  async createAdminUser(
+    @Body(ValidationPipe) createUserDto: CreateUserDto,
+  ): Promise<ReturnUserDto> {
+    const user = await this.usersService.createAdminUser(createUserDto);
+    return { user, message: 'Administrador cadastrado com sucesso' };
+  }
+
+  @Get()
+  async index() {
+    return { status: 'ok' };
+  }
+}
